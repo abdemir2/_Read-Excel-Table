40313633fef332f62fd8dd51ab91322ef1681fc1
Bulent <66696955+abdemir2@users.noreply.github.com>
2025-02-01 23:00:01 +0300

    Test comment

--- Read Excel Table/Program.cs
+++ Read Excel Table/Program.cs
@@ -14,6 +14,7 @@ namespace Read_Excel_Table
 		[STAThread]
 		static void Main()
 		{
+			// my comment
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
