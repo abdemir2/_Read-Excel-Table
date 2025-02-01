4b81ff5eb6fd54517b3dd3fa7a01314f64f5b58b
Bulent Demir <bulentdemir@hotmail.com>
2025-02-01 23:02:28 +0300

    Revert "Test comment"
    This reverts commit 40313633fef332f62fd8dd51ab91322ef1681fc1.

--- Read Excel Table/Program.cs
+++ Read Excel Table/Program.cs
@@ -14,7 +14,6 @@ namespace Read_Excel_Table
 		[STAThread]
 		static void Main()
 		{
-			// my comment
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
